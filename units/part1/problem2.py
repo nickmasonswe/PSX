1974efc6aeaf2007f0accd2a11058e6729d056c4
nickmasonswe <nickmasonswe@gmail.com>
2024-07-30 22:11:17 -0400

    feat: add 3 problems to part1, no tests

---
+++ units/part1/problem2.py
@@ -0,0 +1,9 @@
+#declare a variable named lastName and assign it the value of your last name. Then, without touching any existing code, make it so that when you uncomment the print statement, that it prints your full name.
+
+firstName = "anonymous"
+
+#CODE BELOW
+
+
+#UNCOMMENT BELOW
+#print(firstName + ' ' + lastName)
---
+++ units/part1/problem3.py
@@ -0,0 +1,8 @@
+# Declare a variable favShow and assign it a value of "Succession"
+
+# Now, without editing your code, change the value of favShow to whatever your favorite show is. Next, uncomment the print statement to check your code. You should see "No, but really, my favorite food is " in the console below.
+
+favShow = "Succession"
+
+#UNCOMMENT BELOW
+print("No, but really, my favorite show is" + " " + favShow)
